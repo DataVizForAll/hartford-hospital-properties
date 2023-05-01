436f65df8d6ceba1d4f1090092a02dc36c87428c
Jack Dougherty <jack.dougherty@trincoll.edu>
2023-04-30 21:01:04 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@
 const dataLocation = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQeAqNtXKUax8N2mVRrDRnVUk4eBCnD6kr135mPzLK8M2-7QqBpBJf7hzT23sU2KhkT-5EfO7JNPrqF/pub?output=csv';
 
 // Map's initial center, and zoom level
-const mapCenter = [41.7536, -72.6814];
+const mapCenter = [41.7536, -72.682];
 const mapZoom = 17;
 
 // Marker icon height and width
